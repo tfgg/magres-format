c8e09266d85e2413e5937263aff89c53e29b0298
Timothy Green <timothy.green@gmail.com>
2013-06-19 15:29:19 +0100

    Hack to the setup.py to warn if you don't have .local/bin in your PATH

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 from distutils.core import setup
+import os, site, sys
+
+def bin():
+  return os.path.join(site.USER_BASE, "bin")
+
+def bin_on_path():
+  return bin() in os.environ['PATH'].split(':')
 
 setup(name='Magres format',
       version='0.9',
@@ -10,3 +17,18 @@ setup(name='Magres format',
       packages=['magres', 'magres.schema'],
       scripts=['scripts/convertoldmagres.py', 'scripts/magresjson.py', 'scripts/extract-all-jc-compare.py', 'scripts/compisc.py', 'scripts/magresmerge.py', 'scripts/extract-all-jc.py', 'scripts/absiscdev.py'],
       )
+
+if not bin_on_path() and "--user" in sys.argv[1:]:
+  print "\n\nWARNING: Your scripts directory (%s) is not on your PATH" % bin()
+
+  shell = os.environ["SHELL"].split('/')[-1]
+
+  if shell == "bash":
+    print "You can fix this by adding the following to your .bashrc and reloading your session"
+    print "export PATH=%s:$PATH" % bin()
+  elif shell == "tcsh":
+    print "You can fix this by adding the following to your .tcshrc and reloading your session"
+    print "setenv PATH %s:$PATH" % bin()
+  else:
+    print "You can fix this by adding %s to your PATH environment variable" % bin()
+

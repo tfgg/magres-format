01e5dddc80addd5835674d139b1d22c0345ea439
Timothy Green <timothy.green@gmail.com>
2013-08-27 13:07:34 +0100

    add exp.yaml

--- magres/utils.py
+++ magres/utils.py
@@ -3,6 +3,15 @@ from format import BadVersion
 from atoms import MagresAtoms
 
 def find_all(dir, suffix=".cell"):
+  """
+    Recursively find all files with a particular suffix starting in directory dir. Returns a list of the relative file paths.
+
+    >>> from magres.utils import find_all
+    >>> find_all('.', '.magres')
+    ['./TlCl/TlCl.magres', './Tl(CN)Cl2/Tl(CN)Cl2.magres', './Tl4(OCH3)4/Tl4(OCH3)4.magres', './TlF/TlF.magres', './Tl(CN)2Cl/Tl(CN)2Cl.magres', './TlBr/TlBr.magres', './TlI/TlI.magres', './Tl(CN)3/Tl(CN)3.magres']
+
+  """
+
   calcs = []
   for f in os.listdir(dir):
     path = os.path.join(dir, f)
@@ -14,6 +23,9 @@ def find_all(dir, suffix=".cell"):
   return calcs
 
 def find_all_magres(dir):
+  """
+    Find all magres files starting in directory dir.
+  """
   calcs = []
   for f in os.listdir(dir):
     path = os.path.join(dir, f)
@@ -25,6 +37,10 @@ def find_all_magres(dir):
   return calcs
 
 def load_all_magres(dir):
+  """
+    Find all magres files starting in directory dir and load them into a :py:class:`magres.atoms.MagresAtoms` structure. Returns a list.
+  """
+
   atoms = []
   for magres_file in find_all_magres(dir):
     try:

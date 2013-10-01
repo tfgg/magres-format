dbbef4f1024e3228ce43ddee36b1674cb60c7afa
Timothy Green <timothy.green@gmail.com>
2013-10-01 17:43:30 +0100

    Fiddle with scripts

--- scripts/compisc.py
+++ scripts/compisc.py
@@ -26,8 +26,8 @@ atom1 = atoms.get_species(s1, i1)
 atom2 = atoms.get_species(s2, i2)
 
 if tensor == "isc_spin_total":
-  K1 = atom1.isc_fc[atom2].K + atom1.isc_spin[atom2].K
-  K2 = atom2.isc_fc[atom1].K + atom2.isc_spin[atom1].K
+  K1 = atom1.isc_fc[atom2].K_sym + atom1.isc_spin[atom2].K_sym
+  K2 = atom2.isc_fc[atom1].K_sym + atom2.isc_spin[atom1].K_sym
 else:
   K1 = getattr(atom1, tensor)[atom2].K_sym
   K2 = getattr(atom2, tensor)[atom1].K_sym
